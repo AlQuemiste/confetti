9a16f8b258eca6442b5f6b9eee9f22624dbe56d6
Ammar Nejati <a.nejati@fz-juelich.de>
2022-05-02 18:03:05 +0200

    Shell: Add microphone test and fix guide

--- shell/testmic.sh
+++
@@ -1,6 +0,0 @@
-# https://forums.debian.net/viewtopic.php?t=149268
-
-# list of CAPTURE hardware devices
-arecord -l
-
-arecord --device=plughw:0,0 -d 10 --format S16_LE --rate 16000 -c1 /tmp/testmic.mp3
