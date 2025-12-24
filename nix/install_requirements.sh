e9569928ea352ec165feef3a3c9fa7abf7862b33
AlQuemist <alquemist@Lyriks>
2025-12-25 00:02:39 +0100

    nix/install_requirements.sh: upd

--- nix/install_requirements.sh
+++ nix/install_requirements.sh
@@ -2,21 +2,25 @@
 
 # package requirements
 rq_firmware="firmware-linux firmware-iwlwifi firmware-realtek nvidia-driver"
-rq_build="build-essential emacs cups"
+rq_build="build-essential gfortran gdb git curl clang-format cppcheck emacs"
+rq_py="python3-venv"
+rq_printer="cups"
 rq_pass="keepassxc"
 
 rq_fonts="fonts-inconsolata ttf-bitstream-vera"
-rq_tex="texlive texlive-extra-utils texlive-latex-extra texlive-bibtex-extra cm-super"
+rq_tex="texlive texlive-extra-utils texlive-latex-extra texlive-bibtex-extra texlive-science texlive-publishers texlive-fonts-extra cm-super"
 rq_pdf="pdftk okular evince mupdf mupdf-tools" # okular needs `qt5-style-plugins`
-rq_edit="kate gedit" # kate needs `konsole` for the terminal
+rq_edit="kate" # kate needs `konsole` for the terminal
 rq_img="gpicview libtiff-dev imagemagick kolourpaint"
 # kolourpaint needs `breeze-icon-theme`
 rq_multimedia="ffmpeg mpv"
 rq_xfce="xfce4-battery-plugin xfce4-datetime-plugin
  xfce4-screenshooter xfce4-taskmanager xfce4-weather-plugin
  xfce4-xkb-plugin"
+rq_extra="zeal gimp inkspace maxima"
 
-requirements="$rq_firmware $rq_build
+requirements="$rq_firmware $rq_build $rq_py
+ $rq_printer
  $rq_pass
  $rq_fonts
  $rq_tex
